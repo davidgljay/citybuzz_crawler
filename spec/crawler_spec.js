f53cc77e5be8b0b0bbc2eb36bcae3be189531a2a
David Jay <davidgljay@gmail.com>
2015-10-17 20:17:56 -0400

    Testing crawler.

--- spec/crawler_spec.js
+++ spec/crawler_spec.js
@@ -1,3 +1,7 @@
+var mocks = require('./mocks'),
+nock = require('nock'),
+scope;
+
 var Crawler = require('../crawl');
 
 describe("Crawler", function() {
@@ -5,6 +9,19 @@ describe("Crawler", function() {
 
 	beforeEach(function() {
 		crawler = new Crawler();
-		console.log(crawler.http);
+		scope = nock('http://' + mocks.domain)
+			.get(mocks.path)
+			.reply(200,mocks.page);
 	})
+
+	it("should crawl stuff.", function(done) {
+		var response = crawler.get(mocks.event.records.Sns.Message);
+		response.then(function(res) {
+			expect(res.title).toBe('Just another page');
+			expect(res.body).toBe('This is a mock page with a  link');
+			expect(res.urls[0]).toBe('www.testlink.com/alink/tostuff');
+			expect(res.message).toBe(mocks.event.records.Sns.Message);
+			done();
+		})
+	});
 })
--- spec/mocks.js
+++ spec/mocks.js
@@ -1,20 +1,26 @@
 var mocks= {};
 
-mocks.page="This is a mock page with a <a href='www.testlink.com/alink/tostuff'>link</a>.";
+mocks.page="<html><body><title>Just another page</title><div>This is a mock page with a <a href=\"www.testlink.com/alink/tostuff\">link</a>.</div></body></html>";
+
 mocks.crawler_response = {
 		domain:"www.stuff.com",
 		urls:["www.url1.com", "www.url2.edu"],
 		body:mocks.page
 	}
+
+mocks.domain = "www.stuff.com";
+mocks.path = "/things";
+
 mocks.event = {
 		records: {
 			Sns: {
 				Message: {
-					domain:"www.stuff.com",
-					path:"/things"
+					domain:mocks.domain,
+					path:mocks.path
 				}
 			}
 		}
 	}
+
 
 module.exports = mocks;
